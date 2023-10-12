17328660a148e2a984451fce1f1ecc4f6590fc60
abdul-manan <mahamamanan8@gmail.com>
2023-10-12 09:59:49 +0000

    added fullstackopen_notes and finished courseinfo

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+  const Header = (props) => {
+    return <div>{props.courseName}</div>;
+  };
 
-function App() {
+  const Part = (props) => {
+    return (
+      <p>
+        {props.part} {props.ex}
+      </p>
+    );
+  };
+  const Total = (props) => {
+    return <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header courseName={course} />
+      <Part part={part1} ex={exercises1} />
+      <Part part={part2} ex={exercises2} />
+      <Part part={part3} ex={exercises3} />
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
     </div>
   );
-}
+};
 
 export default App;
